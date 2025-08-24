f054b71d5012e2a03a7ddbed4320499d05129397
Kaan <kaanyurdkl@gmail.com>
2025-08-24 11:00:10 -0400

    Create custom signin page

--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -1,6 +1,9 @@
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
+  pages: {
+    signIn: "/signin",
+  },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
